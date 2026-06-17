896a76b018e71e5cee50d6d4207ad12218852215
Khathutshelo-dev <Khathuflashie@gmail.com>
2026-06-17 16:52:12 +0200

    Refactor Dashboard component to standalone format

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  standalone: true,
+  template: `<h1>Dashboard Works</h1>`
 })
-export class Dashboard {}
+export class DashboardComponent {}
