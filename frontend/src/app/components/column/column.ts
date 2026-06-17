e18a20e9ffaa3b394ededd3ca21f6ffb21d18f5d
Khathutshelo-dev <Khathuflashie@gmail.com>
2026-06-17 12:02:38 +0200

    Delete frontend directory

--- frontend/src/app/components/column/column.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-column',
-  imports: [],
-  templateUrl: './column.html',
-  styleUrl: './column.css',
-})
-export class Column {}
--- frontend/src/app/components/task-card/task-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-task-card',
-  imports: [],
-  templateUrl: './task-card.html',
-  styleUrl: './task-card.css',
-})
-export class TaskCard {}
