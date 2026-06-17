9ebd394e7a3f6954597c5087120091ad5581f81c
Khathutshelo-dev <Khathuflashie@gmail.com>
2026-06-17 18:41:29 +0200

    Refactor DashboardComponent to include authentication
    Implement OnInit lifecycle for user authentication and logout functionality.

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  template: `<h1>Dashboard Works</h1>`
+  templateUrl: './dashboard.html',
+  styleUrls: ['./dashboard.css']
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+
+  userEmail: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      this.router.navigate(['/login']);
+    }
+
+    this.userEmail = 'User';
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
