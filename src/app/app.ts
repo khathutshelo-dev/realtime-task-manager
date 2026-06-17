c4f5a9b6012f04f568bda45f0cc7d48942acba23
Khathutshelo-dev <Khathuflashie@gmail.com>
2026-06-17 16:43:15 +0200

    Refactor App component to use inline template

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
-export class App {}
+export class AppComponent {}
