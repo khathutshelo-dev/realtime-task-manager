be583f7b432ef8bce0645e795c8181452018e15f
Khathutshelo-dev <Khathuflashie@gmail.com>
2026-06-17 16:39:35 +0200

    Refactor Register component and improve registration flow

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,31 +1,38 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css']
 })
-export class Register {
+export class RegisterComponent {
 
   name = '';
   email = '';
   password = '';
+  message = '';
 
   constructor(private auth: AuthService, private router: Router) {}
 
   register() {
-    this.auth.register({
+    const data = {
       name: this.name,
       email: this.email,
       password: this.password
-    }).subscribe(() => {
-      this.router.navigate(['/login']);
+    };
+
+    this.auth.register(data).subscribe({
+      next: (res: any) => {
+        this.message = "Account created successfully ✔";
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        }, 1500);
+      },
+      error: (err) => {
+        this.message = err.error.message || "Registration failed";
+      }
     });
   }
 }
